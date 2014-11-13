028b7a0d5149cc8c998f70a6e0face1891684a13
nfonseca <nelson.chamusca.fonseca@gmail.com>
2014-11-13 17:21:47 +0000

    www

--- index-with-loops.asm
+++ index-with-loops.asm
@@ -1,5 +1,3 @@
-##{0:0:0:index-with-loops.prg}C:\Users\ncfonseca\Documents\GitHub\C64Programming\index-with-loops.asm
-
 ; 10 SYS (49152)
 
 *=$801
@@ -7,10 +5,10 @@
         BYTE    $0E, $08, $0A, $00, $9E, $20, $28,  $34, $39, $31, $35, $32, $29, $00, $00, $00
 
 *=$C000
-        JSR       $E544
+        JSR       $E544 ; clears the screen
         LDX       #0
         LDY       #1
-START     TYA
+START   TYA
         STA       55296,X
         TXA
         STA       1024,X
@@ -18,4 +16,3 @@ START     TYA
         CPX       #255
         BNE       START
         RTS
-
